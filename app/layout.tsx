6467782012387fc1aa24879f777c36e3d4efe161
admin <admin@admins-MacBook-Pro.local>
2025-03-05 11:11:06 -0500

    add meta favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,18 +14,20 @@ export const metadata = {
     template: "%s | Candidato Somos Perú",
   },
   description: "Sitio web oficial del candidato de Somos Perú",
+  applicationName: "Somos Perú",
+  appleWebApp: {
+    title: "Somos Perú",
+    statusBarStyle: "default",
+  },
   icons: [
-    {
-      rel: "icon",
-      type: "image/svg+xml",
-      url: "/logo.svg",
-    },
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      url: "/logo.svg",
-    },
+    { rel: "icon", url: "/favicon.ico", sizes: "any" },
+    { rel: "icon", type: "image/svg+xml", url: "/favicon.svg" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
+    { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+    { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+    { rel: "icon", type: "image/png", sizes: "48x48", url: "/favicon-48x48.png" },
   ],
+  manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({
